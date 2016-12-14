9463bea40d3676a743f4dc7b3c5e13e7a107a8e9
吕其鑫 <17191077671@16.com>
2016-12-14 22:36:53 +0800

    提取公共部分

---
+++ app/movie_list/controller.js
@@ -0,0 +1,54 @@
+(function(angular) {
+	'use strict';
+
+	var module = angular.module('moviecat.movie_list', ['ngRoute', 'moviecat.services.http']);
+
+	module.config(['$routeProvider', function($routeProvider) {
+		$routeProvider.when('/:category/:page', {
+			templateUrl: 'movie_list/view.html',
+			controller: 'MovieListController'
+		});
+	}]);
+
+	module.controller('MovieListController', [
+		'$scope',
+		'$route',
+		'$routeParams',
+		'HttpService',
+		function($scope,$route,$routeParams,HttpService) {
+			//分页
+			var count = 5;
+			var page = parseInt($routeParams.page);
+			var start = (page - 1) * count;
+			$scope.totalpages = 0;
+			$scope.loading = true;
+			$scope.subjects = [];
+			$scope.message = '';
+			$scope.totalCount = 0;
+			$scope.currentPage = page;
+			HttpService.jsonp(
+				'http://api.douban.com/v2/movie/' + $routeParams.category, {
+					start: start,
+					count: count
+				},
+				function(data) {
+					// $apply的作用就是让指定的表达式重新同步
+					$scope.title = data.title;
+					$scope.subjects = data.subjects;
+					$scope.totalCount = data.total;
+					$scope.totalpages = Math.ceil($scope.totalCount / count);
+					$scope.loading = false;
+					$scope.$apply();
+				});
+
+			//上一页下一页
+			$scope.go = function(page) {
+				if (page >= 1 && page <= $scope.totalpages) {
+					$route.updateParams({
+						page: page
+					});
+				}
+			};
+		}
+	]);
+})(angular)
