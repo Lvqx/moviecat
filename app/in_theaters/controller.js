b06b0e47ed70d3d09b1a679735d7dbc9d1eb7d4d
吕其鑫 <17191077671@16.com>
2016-12-14 21:03:33 +0800

    loading and page

--- app/in_theaters/controller.js
+++ app/in_theaters/controller.js
@@ -4,7 +4,7 @@
 	var module = angular.module('moviecat.in_theaters', ['ngRoute', 'moviecat.services.http']);
 
 	module.config(['$routeProvider', function($routeProvider) {
-		$routeProvider.when('/in_theaters', {
+		$routeProvider.when('/in_theaters/:page', {
 			templateUrl: 'in_theaters/view.html',
 			controller: 'InTheaterController'
 		});
@@ -15,14 +15,24 @@
 		'$routeParams',
 		'HttpService',
 		function($scope, $routeParams, HttpService) {
+			//分页
+			var count = 5;
+			var page = parseInt($routeParams.page);
+			var start = (page - 1) * count;
+			$scope.totalpages = 0;
+			$scope.loading = true;
 			$scope.subjects = [];
 			$scope.message = '';
+			$scope.totalCount = 0;
 			HttpService.jsonp(
-				'http://api.douban.com/v2/movie/in_theaters', {},
+				'http://api.douban.com/v2/movie/in_theaters', {start:start,count:count},
 				function(data) {
 					// $apply的作用就是让指定的表达式重新同步
 					$scope.subjects = data.subjects;
-					$scope.$apply('subjects');
+					$scope.totalCount = data.total;
+					$scope.totalpages = Math.ceil( $scope.totalCount / count );
+					$scope.loading = false;
+					$scope.$apply();
 				});
 		}
 	]);
